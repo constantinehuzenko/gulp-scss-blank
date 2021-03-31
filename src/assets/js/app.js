5a875c8d5fb83e90e5ee385a68a9735e34080d8f
costa <kossguzenko@gmail.com>
2021-03-31 13:01:26 +0300

    first commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,18 @@
-console.log(`app.js is working!`);
+const burgerBtn = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const langIcon = document.querySelector('.lang__icon');
+const langList = document.querySelector('.lang__list');
+const navDrop = document.querySelector('.nav__link.nav-drop');
+const navDropList = document.querySelector('.nav__drop');
+
+const toggleClass = (dir, className) => {
+  dir.classList.toggle(className);
+};
+
+const setUpListener = (dir, func) => {
+  dir.addEventListener('click', func);
+};
+
+setUpListener(burgerBtn, () => toggleClass(nav, 'burger-open'));
+setUpListener(langIcon, () => toggleClass(langList, 'lang-open'));
+setUpListener(navDrop, () => toggleClass(navDropList, 'nav__drop-open'));
